8aad9bd34466f22fa41246d03912ed5ea9796788
rickvoorneveld <rvoorneveld@me.com>
2019-10-07 18:57:55 +0200

    Removed doctrine-fixtures-bundle package

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
