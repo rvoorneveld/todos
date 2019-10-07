13955633446460d406cc8eb5750dc44e2dc47776
rickvoorneveld <rvoorneveld@me.com>
2019-10-07 20:17:47 +0200

    Created test for TaskRepository

---
+++ tests/Unit/Repository/TaskRepositoryTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\Task;
+use App\Tests\Traits\InteractsWithDatabase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TaskRepositoryTest extends KernelTestCase
+{
+
+    use InteractsWithDatabase;
+
+    public function testTaskCanBeCreated(): void
+    {
+        $task = ($this->getEntityManager()->getRepository(Task::class))->create($expected = 'foo');
+
+        $this->assertSame($expected, $task->getTitle());
+    }
+
+}
