46a86ab6fff5482d23222f557ef5fe2c83757698
rickvoorneveld <rvoorneveld@me.com>
2019-10-07 20:17:09 +0200

    Moved TaskTest to Entity folder

---
+++ tests/Unit/Entity/TaskTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\Task;
+use App\Tests\Traits\InteractsWithDatabase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TaskTest extends KernelTestCase
+{
+
+    use InteractsWithDatabase;
+
+    public function testAttributesCanBeRetrieved(): void
+    {
+        $task = ($this->getEntityManager()->getRepository(Task::class))->create($title = 'foo');
+
+        $this->assertSame(1, $task->getId());
+        $this->assertSame($title, $task->getTitle());
+    }
+
+}
