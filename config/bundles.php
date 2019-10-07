82cb44bba30262539fc2a523e5ad36c4e8e3435d
rickvoorneveld <rvoorneveld@me.com>
2019-10-07 19:09:14 +0200

    Revert "Remove dama/doctrine-test-bundle package"
    This reverts commit fff55e65033f849fd9d75bd372077217f60c0945.

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
