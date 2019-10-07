aa3f7c519618c168bb1144366c8cfeb747bffbf9
rickvoorneveld <rvoorneveld@me.com>
2019-10-07 20:21:33 +0200

    Test counting of Todos in a single method

--- tests/Integration/TasksController/IndexTest.php
+++ tests/Integration/TasksController/IndexTest.php
@@ -2,17 +2,36 @@
 
 namespace App\Tests\Integration\TasksController;
 
+use App\Entity\Task;
+use App\Tests\Traits\InteractsWithDatabase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class IndexTest extends WebTestCase
 {
 
-    public function testIndexShowsAllTasks(): void
+    use InteractsWithDatabase;
+
+    protected function createTask(string $title): void
+    {
+        ($em = $this->getEntityManager())->persist(
+            (new Task)->setTitle($title)
+        );
+        $em->flush();
+    }
+
+    public function testIndexCountsNumberOfTasks(): void
     {
         $crawler = ($client = static::createClient())->request('GET', '/');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertSame('Show all tasks', $crawler->text());
+        $this->assertStringContainsString('Todos (0)', $crawler->text());
+
+        $this->createTask('foo');
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString('Todos (1)', $text = $crawler->text());
     }
 
 }
