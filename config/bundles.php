fff55e65033f849fd9d75bd372077217f60c0945
rickvoorneveld <rvoorneveld@me.com>
2019-10-07 19:05:19 +0200

    Remove dama/doctrine-test-bundle package

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
